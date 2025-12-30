6dfce99a3e7e06e59631c0f95e81f97907f81cd5
nirmanhansa12-arch <nirmanhansa12@gmail.com>
2025-12-30 19:10:38 +0530

    Update settings.js

--- settings.js
+++ settings.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "Your_Session_Id", // Add your session id
+SESSION_ID: process.env.SESSION_ID || "3RQxRbra#T71f25EY9FzSGmdukI6q16eB2Bgtx2w3XoAxvdOiRqE", // Add your session id
 PREFIX: process.env.PREFIX || ".",
 MODE: process.env.MODE || "public",  //public | private | inbox | group
 OWNER_NUMBER: process.env.OWNER_NUMBER || "94779062397",
